228e033cd7fe6153f24972a9d1e430c07bd22e81
yoAlbireo <robert.ks.email@gmail.com>
2021-04-30 22:11:24 -0400

    Build Finished

---
+++ Furniture VR/Assets/Scripts/SnapTurning.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapTurning : MonoBehaviour
+{
+    public GameObject VRRig;
+    // Start is called before the first frame update
+    public void TurnLeft()
+    {
+        
+    }
+}
